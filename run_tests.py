3c8220eba4d9ceafe5a2fb73eb0c7e8b4a6cc05e
Anton Malakhov <Anton.Malakhov@intel.com>
2017-04-16 20:58:09 -0500

    Reducing time in the benchmark

--- run_tests.py
+++ run_tests.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('miniconda3/envs/intel3/bin/python numpy/umath/umath_mem_bench.py -s 100')
+os.system('miniconda3/envs/intel3/bin/python numpy/umath/umath_mem_bench.py -v --size 10 --goal-time 0.01 --repeats 1')
