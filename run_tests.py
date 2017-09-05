e3db229a519aa7926fcce5c9fafaa25d661c2dc8
Anton <anton-malakhov@users.noreply.github.com>
2017-09-05 16:15:13 -0500

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,3 +1,3 @@
 import os
-
+# warning: this is sanity test for Travis CI. The arguments are really bad for real perf testing, use default arguments instead
 os.system('miniconda3/envs/intel3/bin/python numpy/umath/umath_mem_bench.py -v --size 10 --goal-time 0.01 --repeats 1')
