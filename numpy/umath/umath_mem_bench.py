b57782b907c4759448b5e64fe5d5949b21f65c93
Anton Malakhov <Anton.Malakhov@intel.com>
2018-03-13 22:01:54 -0500

    Added Numba implementation

--- numpy/umath/umath_mem_bench.py
+++ numpy/umath/umath_mem_bench.py
@@ -24,6 +24,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
+import os
 import sys
 import platform
 import warnings
@@ -55,7 +56,7 @@ def_funcs = ['+', '-', '*', '/', 'sin', 'cos', 'tan', 'sinh', 'cosh', 'tanh',
     #'fabs', 'floor', 'ceil', 'rint', 'trunc',
     'sqrt', 'log10', 'log', 'exp', 'expm1', 'arcsin', 'erf',
     'arccos', 'arctan', 'arcsinh', 'arccosh', 'arctanh', 'log1p', 'exp2', 'log2', 'copyto']
-def_impls = ['numpy', 'numexpr']
+def_impls = ['numpy', 'numexpr', 'numba']
 
 argParser.add_argument('-l', '--log',       default=None,      help="log")
 argParser.add_argument('-p', '--prefix',    default='@',       help="prefix string")
@@ -65,6 +66,7 @@ argParser.add_argument('-m', '--impl',      default=def_impls, help="implementat
 argParser.add_argument('-g', '--goal-time', default=1,         help="goal for measured time in ms")
 argParser.add_argument('-r', '--repeats',   default=30,        help="repeat experements and get minimum time")
 argParser.add_argument('-o', '--offsets',   default=(0,1,2,4), help="Offset from aligned in elements", nargs='+', type=int)
+argParser.add_argument(    '--fast-math',   default=False,     help="Enable fast math mode", action="store_true")
 argParser.add_argument('-v', '--verbose',   default=False,     help="print additonal info", action="store_true")
 args = argParser.parse_args()
 assert type(args.func) is list
@@ -75,43 +77,62 @@ for impl in args.impl:
     if impl == "numexpr":
         import numexpr
     if impl == "numba":
-        import numba
+        import numba, math
+        if "OMP_NUM_THREADS" in os.environ:
+            run_par = numba.config.NUMBA_DEFAULT_NUM_THREADS = int(os.environ["OMP_NUM_THREADS"])
+        run_par = numba.config.NUMBA_DEFAULT_NUM_THREADS > 1
 
 goalTime = float(args.goal_time)/1000.
 scalararraytypes = range(0, 3)
 np_types = [np.float64]
 overheadMin = 0
 
+def numbaKernel(params, code, sigs):
+    n = "numba_%X"%abs(hash(code))
+    r = "numba.prange" if run_par else "range"
+    exec("""def {n}({params},out):
+              for i in {r}(out.size):
+                out[i] = {code}
+         """.format(**locals()))
+    return numba.njit(sigs, parallel=run_par, fastmath=args.fast_math)(locals()[n])
 
-def getFuncImpl(func, impl):
-  if len(func) <= 2: # binary
+def getBinaryFuncImpl(func, impl, scalar):
     if impl == "numexpr":
-        f = lambda x,y,out: numexpr.evaluate("x %s y"%func, out=out)
+        return lambda x,y,out: numexpr.evaluate("x %s y"%func, out=out)
     elif impl == 'numpy':
-        f = {'+': np.add, '*': np.multiply, '/':  np.true_divide, '-': np.subtract}[func]
-    else:
-        raise NotImplementedError(impl)
-    return f, func
-  else: # unary
+        return {'+': np.add, '*': np.multiply, '/':  np.true_divide, '-': np.subtract}[func]
+    elif impl == 'numba':
+      if scalar == 0:
+        return numbaKernel("x,y", "x[i] %s y[i]"%func, "(f8[::1],f8[::1],f8[::1])")
+      elif scalar == 1:
+        return numbaKernel("x,y", "x[i] %s y"%func, "(f8[::1],f8,f8[::1])")
+      elif scalar == 2:
+        return numbaKernel("x,y", "x %s y[i]"%func, "(f8,f8[::1],f8[::1])")
+    raise NotImplementedError(impl)
+
+def getUnaryFuncImpl(func, impl):
     if impl == "numexpr":
         if func == 'invsqrt':
-            f = lambda x,out: numexpr.evaluate("1/sqrt(x)", out=out)
+            return lambda x,out: numexpr.evaluate("1/sqrt(x)", out=out)
         else:
-            f = lambda x,out: numexpr.evaluate("%s(x)"%func, out=out)
+            return lambda x,out: numexpr.evaluate("%s(x)"%func, out=out)
+    elif impl == 'numba':
+        if func == 'invsqrt':
+            return numbaKernel("x", "1./math.sqrt(x[i])", "(f8[::1],f8[::1])")
+        else:
+            return numbaKernel("x", "math.%s(x[i])"%func, "(f8[::1],f8[::1])")
     elif impl == 'numpy':
         try:
-            f = getattr(np.core.umath, func, getattr(np, func))
+            return getattr(np.core.umath, func, getattr(np, func))
         except:
             if func == "erf":
                 from scipy.special import erf
-                f = erf
+                return erf
             elif func == "invsqrt":
-                f = lambda x, y: 1/np.sqrt(x, y)
+                return lambda x, y: 1/np.sqrt(x, y)
             else:
                 raise
-    else:
-        raise NotImplementedError(impl)
-    return f, False
+    raise NotImplementedError(impl)
 
 
 def emptyF(x, y):
@@ -178,7 +199,7 @@ def clOffset(a):
 
 overheadMin = runBench(emptyF, 0, 0, internalCount=100000, overhead=0)
 print("Overhead time per loop iteration = ",  overheadMin, " clock = ", clock_name)
-print("Prefix, Implementation, Function, Type, Iterations, Size, CPE:aligned, CPE:max")
+print("Prefix,Implementation,Function,Type,Iterations,Size,CPE:aligned,CPE:max")
 
 for np_type in np_types:
   zoffsets = xoffsets = yoffsets = args.offsets
@@ -205,11 +226,18 @@ for np_type in np_types:
     for impl in args.impl:
       for func in args.func:
         try:
-            np_func, op = getFuncImpl(func, impl)
-            if op: # binary operation
+            if len(func) <= 2: # binary operation
+              op = func
               CPEs = np.zeros([znoffs,xnoffs,ynoffs])
               for scalararraytype in scalararraytypes:
-                internalCount, internalTime = getInternalCount(np_func, z0, x0, y0)
+                np_func = getBinaryFuncImpl(func, impl, scalararraytype)
+                if scalararraytype == 0:
+                    x,y,z = x0,y0,z0
+                elif scalararraytype == 1:
+                    x,y,z = x0,y0[0],z0
+                elif scalararraytype == 2:
+                    x,y,z = x0[0],y0,z0
+                internalCount, internalTime = getInternalCount(np_func, z, x, y)
                 for zi in reversed(range(znoffs)):
                   for xi in reversed(range(xnoffs)):
                     for yi in reversed(range(ynoffs)):
@@ -231,14 +259,14 @@ for np_type in np_types:
                             y = y[0]
                         elif scalararraytype == 2:
                             satype = 'C[%d:]=scalar%sA[%d:]' % (zoff, op, yoff)
-                            y = x
-                            x = y[0]
+                            #y = x  y=y[0]
+                            x = x[0]
 
                         CPA_min = runBench(np_func, z, x, y, internalCount=internalCount)
                         CPE_min = np.true_divide(CPA_min, n)
                         CPEs[zi][xi][yi] = CPE_min
                         if args.verbose:
-                            print(args.prefix, impl, satype, np_type.__name__, '% 6d'%internalCount, '% 7d'%n, '% 4.2f'%CPE_min, sep=', ', flush=True)
+                            print(args.prefix, '% 7s'%impl, satype, np_type.__name__, '% 6d'%internalCount, '% 7d'%n, '% 4.2f'%CPE_min, sep=', ', flush=True)
                 if scalararraytype == 0:
                     satype = ' array%sarray' % op
                 elif scalararraytype == 1:
@@ -246,8 +274,9 @@ for np_type in np_types:
                 elif scalararraytype == 2:
                     satype = 'scalar%sarray' % op
                 checkResults(CPEs)
-                print(args.prefix, impl, satype, np_type.__name__, '% 7d'%internalCount, '% 7d'%n, '% 6.2f'%CPEs[0][0][0], '% 6.2f'%np.max(CPEs), sep=', ', flush=True)
+                print(args.prefix, '% 7s'%impl, '% 12s'%satype, np_type.__name__, '% 7d'%internalCount, '% 7d'%n, '% 6.2f'%CPEs[0][0][0], '% 6.2f'%np.max(CPEs), sep=', ', flush=True)
             else: # unary operation
+                np_func = getUnaryFuncImpl(func, impl)
                 CPEs = np.zeros([znoffs,xnoffs])
                 a0 = y0 if func in ['arccosh'] else x0 # otherwise it results in a complex number
                 internalCount, internalTime = getInternalCount(np_func, z0, a0)
@@ -267,8 +296,8 @@ for np_type in np_types:
                     CPEs[zi][xi] = CPE_min
                     if args.verbose:
                         satype = 'c[%d:]=% 7s(a[%d:])'%(clOffset(z)/z.itemsize, func, clOffset(x)/x.itemsize)
-                        print(args.prefix, impl, satype, np_type.__name__, '% 6d'%internalCount, '% 7d'%n, '% 4.2f'%CPE_min, sep=', ', flush=True)
+                        print(args.prefix, '% 7s'%impl, satype, np_type.__name__, '% 6d'%internalCount, '% 7d'%n, '% 4.2f'%CPE_min, sep=', ', flush=True)
                 checkResults(CPEs)
-                print(args.prefix, impl, '% 7s'%func, np_type.__name__, '% 7d'%internalCount, '% 7d'%n, '% 6.2f'%CPEs[0][0], '% 6.2f'%np.max(CPEs), sep=', ', flush=True)
+                print(args.prefix, '% 7s'%impl, '% 12s'%func, np_type.__name__, '% 7d'%internalCount, '% 7d'%n, '% 6.2f'%CPEs[0][0], '% 6.2f'%np.max(CPEs), sep=', ', flush=True)
         except Exception as e:
             print("Failed while executing %s for %s: %s" % (func, impl, str(e)))
